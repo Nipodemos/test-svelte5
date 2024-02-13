027b550a5c397453b33f04fbb62386bda153457f
Nipodemos <nipodemos13@gmail.com>
2024-02-12 23:27:35 -0300

    teste superforms

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { superForm } from 'sveltekit-superforms';
+	import SuperDebug from 'sveltekit-superforms';
+
+	export let data;
+
+	// Client API:
+	const { form, errors, constraints, enhance } = superForm(data.form);
+</script>
+
+<SuperDebug data={$form} />
+<form method="POST" use:enhance style="display: flex">
+	<label for="name">Name</label>
+	<input
+		type="text"
+		name="name"
+		aria-invalid={$errors.name ? 'true' : undefined}
+		bind:value={$form.name}
+		{...$constraints.name}
+	/>
+	{#if $errors.name}<span class="invalid">{$errors.name}</span>{/if}
+
+	<label for="email">E-mail</label>
+	<input
+		type="email"
+		name="email"
+		aria-invalid={$errors.email ? 'true' : undefined}
+		bind:value={$form.email}
+		{...$constraints.email}
+	/>
+	{#if $errors.email}<span class="invalid">{$errors.email}</span>{/if}
+
+	<div><button>Submit</button></div>
+</form>
+
+<style>
+	.invalid {
+		color: red;
+	}
+</style>
